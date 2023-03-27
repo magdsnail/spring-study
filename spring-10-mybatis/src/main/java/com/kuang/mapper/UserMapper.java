7a2bb32771eae28c3d0752cedfad3d75365037bd
wangkejing <wangkjim@gmail.com>
2023-03-27 09:45:32 +0800

    spring-mybais

---
+++ spring-10-mybatis/src/main/java/com/kuang/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.kuang.mapper;
+
+/**
+ * ClassName: UserMapper
+ * Package: com.kuang.mapper
+ * Description:
+ *
+ * @Date: 2023-03-25 025 11:40
+ * @Author: wangkejing
+ */
+public interface UserMapper {
+}
---
+++ spring-10-mybatis/src/main/java/com/kuang/pojo/User.java
@@ -0,0 +1,18 @@
+package com.kuang.pojo;
+
+import lombok.Data;
+
+/**
+ * ClassName: User
+ * Package: com.kuang.pojo
+ * Description:
+ *
+ * @Date: 2023-03-25 025 11:17
+ * @Author: wangkejing
+ */
+@Data
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+}
